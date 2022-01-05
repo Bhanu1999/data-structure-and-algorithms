e3fad2516c403cfd65fe638d2934428542686b86
bhanu1999 <sakinalabhanu99@gmail.com>
2022-01-06 01:09:07 +0530

    searchMatrix

---
+++ algorithms/sub_array_sum_equaltok.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<unordered_map>
+#include<queue>
+#include<stack>
+#include<math.h>
+#include<set>
+
+using namespace std;
+
+void print(int a[], int n) {
+
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+void print(vector<int> a) {
+    int n = a.size();
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;//
+}
+
+int32_t main() {
+vector<int> v{1,2,3,4,5};
+vector<vector<int>> ans;
+
+
+}
