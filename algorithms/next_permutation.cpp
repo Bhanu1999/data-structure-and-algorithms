13151f4ddde6b181df4d63cdd188a05a4f61cb8d
bhanu1999 <sakinalabhanu99@gmail.com>
2022-01-13 23:50:52 +0530

    jump game 1

---
+++ algorithms/next_permutation.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<unordered_map>
+#include<queue>
+#include<stack>
+#include<math.h>
+#include<set>
+#include <algorithm>
+
+using namespace std;
+
+void print(int a[], int n) {
+
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+void print(vector<int> a) {
+    int n = a.size();
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+int32_t main() {
+vector<int> v{1,2,3,5};
+int i=0;
+while (i++<24)
+{
+    print(v);
+    next_permutation(v.begin(),v.end());
+}
+
+
+}
