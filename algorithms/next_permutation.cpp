928ddf3dca51e609292cc59a3956a9d8914803d7
bhanu1999 <sakinalabhanu99@gmail.com>
2022-01-14 17:37:15 +0530

    jump game 1

--- algorithms/next_permutation.cpp
+++ algorithms/next_permutation.cpp
@@ -28,13 +28,37 @@ void print(vector<int> a) {
 
 int32_t main() {
 vector<int> v{1,2,3,5};
-int i=0;
-while (i++<24)
-{
-    //
-    print(v);
-    next_permutation(v.begin(),v.end());
-}
+
+    int n=v.size();
+    int ind1=n-1;
+    int i=ind1;
+    while(i>0)
+    {
+        if(v[i]>v[i-1])
+        {
+            ind1=i-1;
+            break;
+        }
+        i--;
+    }
+    if(i==0)
+    {
+        reverse(v.begin(),v.end());
+        return 0;
+    }
+    int ind2=n-1;
+    int a=v[ind1];
+    while(ind2>=0)
+    {
+        if(v[ind2]>a)
+        {
+            break;
+        }
+        ind2--;
+
+    }
+    swap(v[ind1],v[ind2]);
+    reverse(v.begin()+ind1+1,v.end());
 
 
 }
