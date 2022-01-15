8008277c1df1828fd9a05924f99101321bac0dd8
bhanu1999 <sakinalabhanu99@gmail.com>
2022-01-15 20:46:31 +0530

    jump game 1

--- algorithms/152.MaximumProductSubarray.cpp
+++ algorithms/152.MaximumProductSubarray.cpp
@@ -26,6 +26,33 @@ void print(vector<int> a) {
 }
 
 int32_t main() {
+    vector<int> v{2,3,-2,4};
+    int ans=v[0];
+    int n=v.size();
+    for(int i=0;i<n;i++)
+    {
+        ans=max(ans,v[i]);
+    }
+    int min_ans=1;
+    int max_ans=1;
+    for(int i=0;i<n;i++)
+    {
+        if(v[i]==0)
+        {
+            min_ans=1;
+            max_ans=1;
+            continue;
+        }
+        else
+        {
+            int temp=max_ans;
+            max_ans=max(max_ans*v[i],max(min_ans*v[i],v[i]));
+            min_ans=min(min_ans*v[i],min(temp*v[i],v[i]));
 
+        }
+        ans=max(ans,max_ans);
+    }
+
+    return ans;
 
 }
