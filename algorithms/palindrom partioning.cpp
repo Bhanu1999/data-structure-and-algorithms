1c3535054f389af66cf9f9c53c36f569b9e5db33
bhanu1999 <sakinalabhanu99@gmail.com>
2022-02-17 12:01:12 +0530

    abc

---
+++ algorithms/palindrom partioning.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<unordered_map>
+#include<queue>
+#include<stack>
+#include<math.h>
+#include<set>
+
+using namespace std;
+
+vector<vector<string>> ans;
+void findPartitions(string s,int ind,vector<string> temp)
+{
+    if(ind==s.size())
+    {
+        ans.push_back(temp);
+        return;
+    }
+    for(int i=ind;i<s.size();i++)
+    {
+        if(is(s.substr(ind,i-ind+1)))
+        {
+            temp.push_back(s.substr(ind,i-ind+1));
+            findPartitions(s,i+1,temp);
+            temp.pop_back();
+        }
+    }
+    return;
+}
+bool is(string s)
+{
+    int i=0;
+    int j=s.size()-1;
+    while(i<j)
+    {
+        if(s[i]!=s[j])
+            return false;
+        i++;
+        j--;
+    }
+    return true;
+}
+vector<vector<string>> partition(string s) {
+    vector<string> temp;
+    findPartitions(s,0,temp);
+    return ans;
+}
+
+int32_t main() {
+
+
+}
