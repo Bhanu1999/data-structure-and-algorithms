1f11f776dc1cf4a7e6456731fcf42c41f1380676
bhanu1999 <sakinalabhanu99@gmail.com>
2022-02-22 17:41:42 +0530

    kth permutation sequence

---
+++ striver-sde-sheet/kth permutation.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<unordered_map>
+#include<queue>
+#include<stack>
+#include<math.h>
+#include<set>
+
+using namespace std;
+
+void print(int a[], int n) {
+
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+void print(vector<int> a) {
+    int n = a.size();
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+//hi
+
+int32_t main() {
+
+
+}
