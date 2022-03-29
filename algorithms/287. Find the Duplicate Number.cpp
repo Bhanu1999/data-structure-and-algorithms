2695cfbf04ba0d368e6d07829f1e6fcf806941bb
bhanu1999 <sakinalabhanu99@gmail.com>
2022-03-29 12:26:56 +0530

    287. find the duplicate number

---
+++ algorithms/287. Find the Duplicate Number.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<unordered_map>
+#include<queue>
+#include<stack>
+#include<math.h>
+#include<set>
+
+using namespace std;
+
+void print(int a[], int n) {
+
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+void print(vector<int> a) {
+    int n = a.size();
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+int findDuplicate(vector<int>& v) {
+    for(int i=0;i<v.size();i++)
+    {
+        if(v[abs(v[i])-1]<0)
+        {
+            return abs(v[i]);
+        }
+        v[abs(v[i])-1]*=(-1);
+    }
+    return 0;
+}
+int32_t main() {
+
+
+}
