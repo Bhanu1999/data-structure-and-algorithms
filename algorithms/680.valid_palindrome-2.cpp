c29430035ca16c10affebe68fb2e5bd3c5460f6d
bhanu1999 <sakinalabhanu99@gmail.com>
2022-04-03 13:55:15 +0530

    leetcode biweekly contest

--- algorithms/680.valid_palindrome-2.cpp
+++ algorithms/680.valid_palindrome-2.cpp
@@ -52,6 +52,40 @@ bool validPalindrome(string s) {//
     }
     return true;
 }
+int convertTime(string current, string correct) {
+    int h1=((current[0]-'0')*10+(current[1]-'0'));
+    int h2=((correct[0]-'0')*10+(correct[1]-'0'));
+    int m1=((current[3]-'0')*10+(current[4]-'0'));
+    int m2=((correct[3]-'0')*10+(correct[4]-'0'));
+
+    int total1=h1*60+m1;
+    int total2=h2*60+m2;
+    cout<<total1<<total2;
+    int ans=0;
+    while(total2-total1>=60)
+    {
+        ans++;
+        h1+=60;
+    }
+    while(total2-total1>=15)
+    {
+        ans++;
+        h1+=15;
+    }
+    while(total2-total1>=5)
+    {
+        ans++;
+        h1+=5;
+    }
+    while(total2-total1>1)
+    {
+        ans++;
+        h1+=1;
+    }
+
+    return ans;
+
+}
 int32_t main() {
 
 
