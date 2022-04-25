23f508a8f755afb4009166f2592fc0ef882563b5
bhanu1999 <sakinalabhanu99@gmail.com>
2022-04-25 22:32:16 +0530

    design hashset

--- algorithms/temp.cpp
+++ algorithms/temp.cpp
@@ -25,10 +25,72 @@ void print(vector<int> a) {
     }
     cout << endl;
 }
+int countLatticePoints(vector<vector<int>>& circles) {
+    set<pair<int,int>> ans;
+    for(int i=0;i<circles.size();i++)
+    {
+        vector<int> temp=circles[i];
+        int dist=temp[2];
+        int temp_dist=1;
+        int le=temp[0]-temp[2];
+        int re=temp[0]+temp[2];
+        while(le<=re)
+        {
+            ans.insert(make_pair(le,temp[1]));
+            le++;
+        }
+        le=temp[1]-temp[2];
+        re=temp[1]+temp[2];
+        while(le<=re)
+        {
+            ans.insert(make_pair(temp[1],le));
+            le++;
+        }
+        int ind1=temp[0];
+        int ind2=temp[i];
+        while(temp_dist<dist)
+        {
+            ind1--;
+            ind2--;
+            ans.insert(make_pair(ind1,ind2));
+            temp_dist++;
+        }
+        temp_dist=1;
+        while(temp_dist<dist)
+        {
+            ind1++;
+            ind2--;
+            ans.insert(make_pair(ind1,ind2));
+            temp_dist++;
+        }
+        temp_dist=1;
+        while(temp_dist<dist)
+        {
+            ind1++;
+            ind2++;
+            ans.insert(make_pair(ind1,ind2));
+            temp_dist++;
+        }
+        temp_dist=1;
+        while(temp_dist<dist)
+        {
+            ind1--;
+            ind2++;
+            ans.insert(make_pair(ind1,ind2));
+            temp_dist++;
+        }
 
+
+
+    }
+    for(auto i:ans)//
+    {
+        cout<<i.first<<" "<<i.second<<endl;
+    }
+    return ans.size();
+}
 
 int main() {
-    vector<int> nums{0,10,0,3,0};//
 
 
 
