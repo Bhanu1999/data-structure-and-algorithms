bcacd967bea401c31aeca8fca1fa0c1c177c83f1
bhanu1999 <sakinalabhanu99@gmail.com>
2022-09-06 17:09:50 -0400

    1641. Count Sorted Vowel Strings.cpp

---
+++ pappu.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<unordered_map>
+#include<queue>
+#include<stack>
+#include<math.h>
+#include<set>
+
+using namespace std;
+
+void print(int a[], int n) {
+
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+void print(vector<int> a) {
+    int n = a.size();
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+int32_t main() {
+
+
+}
